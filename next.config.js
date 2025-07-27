2417774a33667418836bcfe92d54295a4d3e7e2e
Mohamed Siedahmed <siedahmedmohamed@gmail.com>
2025-07-26 20:31:39 -0700

    Fix config file

--- next.config.js
+++ next.config.js
@@ -38,8 +38,8 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  serverExternalPackages: ['playwright', 'puppeteer'],
   experimental: {
+    serverComponentsExternalPackages: ['playwright', 'puppeteer'],
     optimizeCss: true,
     scrollRestoration: true,
   },
